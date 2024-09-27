002ea77a8e8b90ffa73d7298acfd8a0423f7e6f6
Elliot Voris <elliot@stellar.org>
2024-09-26 21:58:19 -0500

    add a console statement on page load

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,6 +3,8 @@ import helloWorld from '$lib/contracts/hello_world';
 
 export const load = (async () => {
     const { result } = await helloWorld.hello({ to: 'Silly Goose' });
+    console.log('Got smart contract result:', result)
+
     return {
         greeting: result,
     };
